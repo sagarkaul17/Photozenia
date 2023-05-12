b5b61ec326bd5b87275a747b38bb69a663ce3107
Sagar Kaul <kaul.sagar@indiamart.com>
2023-05-12 12:57:15 +0530

    changes

--- src/components/ImageModal.js
+++ src/components/ImageModal.js
@@ -5,9 +5,9 @@ const ImageModal = ({imgSrc, closeModal}) => {
         <div className="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
             <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
             <div className="fixed inset-0 z-10 overflow-y-auto">
-                <div className="flex min-h-full items-center justify-center p-4 text-center sm:items-center sm:p-0">
-                    <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                        <div className="bg-white px-4 pb-4 sm:p-6 sm:pb-4">
+                <div className="flex min-h-full items-center justify-center p-4 text-center">
+                    <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all">
+                        <div className="bg-white p-4 pb-4">
                             <div className="flex justify-center items-center">
                             <button onClick={closeModal} type="button" className="absolute top-0 right-0 m-2 rounded-md bg-red-600 px-2 py-0 text-xl font-semibold text-white shadow-sm hover:bg-red-500">&#215;</button>
                                 <img className='w-fit h-fit' src={imgSrc}/>
